966609083099033e8036b847dbceb61ad16fa839
hufeng <loveruby1988@qq.com>
2016-02-29 12:39:14 +0800

    add connect-to-store

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import connectToStore from './lib/connect-to-store';
+
 module.exports = {
+  connectToStore,
   Store: require('./lib/store'),
   Validator: require('./lib/validator'),
   msg: require('./lib/msg'),
   mixins: {
-    StoreMixin: require('./lib/mixins/store-mixin')	
-  } 
+    StoreMixin: require('./lib/mixins/store-mixin')
+  }
 };
